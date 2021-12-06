17c12578d5224e9e93b5626365d4c20a571376a4
sujun <2896026047@qq.com>
2021-12-06 10:27:27 +0800

    new

---
+++ src/main/java/com/jun/jenkins/Main.java
@@ -0,0 +1,14 @@
+package com.jun.jenkins;
+
+/**
+ * @author Jun
+ * @date 2021/12/6
+ * @apiNote
+ */
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            System.out.println("hello world");
+        }
+    }
+}
